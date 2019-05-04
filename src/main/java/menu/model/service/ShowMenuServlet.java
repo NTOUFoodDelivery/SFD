c50a4582874012150bb2842ef225f3ef8c17dec2
陸顥壬 <f2212671555@gmail.com>
2019-05-04 13:45:02 +0800

    heroku service test

--- src/main/java/menu/model/service/ShowMenuServlet.java
+++ src/main/java/menu/model/service/ShowMenuServlet.java
@@ -1,16 +1,34 @@
 package menu.model.service;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import order.model.javabean.PushResult;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ShowMenuServlet")
 public class ShowMenuServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        Gson gson = new GsonBuilder().disableHtmlEscaping().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
 
+        PushResult result = new PushResult();
+        result.setOrderID("test");
+        result.setAccept(true);
+
+        String json = gson.toJson(result);
+        PrintWriter out = response.getWriter();
+        out.print(json);
+        out.flush();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
