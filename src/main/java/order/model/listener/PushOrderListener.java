48fca74ad5003f4249a2020527e0a8253306b55b
陸顥壬 <f2212671555@gmail.com>
2019-05-04 14:48:29 +0800

    web.xml session setting 30 min 回收

---
+++ src/main/java/order/model/listener/PushOrderListener.java
@@ -0,0 +1,35 @@
+package order.model.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+@WebListener()
+public class PushOrderListener implements ServletContextListener,
+        HttpSessionListener, HttpSessionAttributeListener {
+
+    // Public constructor is required by servlet spec
+    public PushOrderListener() {
+    }
+
+    // -------------------------------------------------------
+    // ServletContextListener implementation
+    // -------------------------------------------------------
+    public void contextInitialized(ServletContextEvent sce) {
+      /* This method is called when the servlet context is
+         initialized(when the Web application is deployed). 
+         You can initialize servlet context related data here.
+      */
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+      /* This method is invoked when the Servlet Context 
+         (the Web application) is undeployed or 
+         Application Server shuts down.
+      */
+    }
+}
