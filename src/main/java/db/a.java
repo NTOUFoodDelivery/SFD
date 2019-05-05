1d0655d86153c75de7223a8ac53985bf33f4f62e
JAJA <JAJA@LAPTOP-KVCQFJ1E>
2019-05-06 00:12:58 +0800

    connect to heroku database test

--- src/main/java/db/a.java
+++ src/main/java/db/a.java
@@ -8,7 +8,8 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class a 
+
+public class a
 {
 	private Connection con = null;  //Database objects
 	private PreparedStatement pst = null;
@@ -24,11 +25,14 @@ public class a
 	public a() {
 		try { 
 		      Class.forName("com.mysql.cj.jdbc.Driver"); 
+			//Class.forName("com.mysql.jdbc.Driver"); 
 		      //註冊driver 
 		      con = DriverManager.getConnection( 
-			"jdbc:mysql://127.0.0.1:3306/sfd_db?"
+			//"jdbc:mysql://127.0.0.1:3306/sfd_db?"
+		    "jdbc:mysql://b13568559a2078:c511925d@us-cdbr-iron-east-02.cleardb.net/heroku_9519aa67df829c7?"
 			+ "serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=UTF-8", 
-			"root","jaja121605");
+			//"root","jaja121605");
+			"b13568559a2078"," c511925d");
 		      //取得connection
 
 		//jdbc:mysql://localhost/test?useUnicode=true&characterEncoding=Big5
@@ -56,7 +60,7 @@ public class a
 			pst.setString(3, "蘇包");
 			pst.setString(4, "bao@gmail.com");
 			pst.setString(5, "0978716315");
-			pst.setString(6, null);
+			pst.setString(6, "smail taipei");
 			pst.setString(7, "EAT");
 			pst.setString(8, "fuck");
 			pst.executeUpdate();
@@ -80,6 +84,8 @@ public class a
 			//pst.setString(2, "User_Id");
 			pst.setString(1, "b00000");
 			rs = pst.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData(); //取得Query資料
+			int numColumns = rsmd.getColumnCount();
 			while(rs.next()) {
 				na = rs.getString("User_Id");
 				id = rs.getString("User_Name");
@@ -113,6 +119,7 @@ public class a
 	public static void main(String args[]) {
 		a f = new a();
 		f.searchTable("a00000");
+		//f.insertTable();
 	}
 	
 }
