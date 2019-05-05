70cafb96622d43cfab175ee10eb5b296ab5cf094
陸顥壬 <f2212671555@gmail.com>
2019-05-05 12:32:15 +0800

    filter 和 多次讀取流

--- src/main/java/filter/CommonRequestFilter.java
+++ src/main/java/filter/CommonRequestFilter.java
@@ -4,12 +4,16 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(filterName = "CommonRequestFilter")
+@WebFilter("/CommonRequestFilter")
 public class CommonRequestFilter implements Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+
+
         chain.doFilter(req, resp);
     }
 
--- src/main/java/member/model/filter/LogFilter.java
+++ src/main/java/member/model/filter/LogFilter.java
@@ -4,7 +4,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(filterName = "LogFilter")
+@WebFilter("/LogFilter")
 public class LogFilter implements Filter {
     public void destroy() {
     }
