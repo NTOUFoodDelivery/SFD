88c4ebefb6330bd59adfba6cfe98211f5f0d03e2
陸顥壬 <f2212671555@gmail.com>
2019-05-11 04:21:06 +0800

    junit setting v1

---
+++ src/test/java/menu/controller/service/ShowRestInfoServletTest.java
@@ -0,0 +1,24 @@
+package menu.controller.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShowRestInfoServletTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    @Test
+    public void doPost() {
+    }
+
+    @Test
+    public void doGet() {
+    }
+}
---
+++ src/test/java/order/controller/service/SendOrderServletTest.java
@@ -0,0 +1,25 @@
+package order.controller.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SendOrderServletTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void doPost() {
+    }
+
+    @Test
+    public void doGet() {
+    }
+}
