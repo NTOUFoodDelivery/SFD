b436f91f7ded5513f0902b5f2cc43100b3bea741
陸顥壬 <f2212671555@gmail.com>
2019-05-12 01:37:10 +0800

    sign up servlet

--- src/main/java/order/controller/service/SendOrderServlet.java
+++ src/main/java/order/controller/service/SendOrderServlet.java
@@ -18,8 +18,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
-import static tool.HttpCommonAction.getRequestBody;
-
 @WebServlet("/SendOrderServlet")
 public class SendOrderServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
