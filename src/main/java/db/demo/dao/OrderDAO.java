80d2c56d0b0b6d1876f093d1cb0d59bd0955d68a
JAJA <JAJA@LAPTOP-KVCQFJ1E>
2019-05-12 16:04:24 +0800

    歷史訂單查詢完成(食物已FOODID輸出需再轉換)

--- src/main/java/db/demo/dao/OrderDAO.java
+++ src/main/java/db/demo/dao/OrderDAO.java
@@ -3,10 +3,16 @@ package db.demo.dao;
 import db.demo.connect.JdbcUtils;
 import db.demo.javabean.User;
 import order.model.javabean.Order;
+//import testsfd_db.connect;
+import tool.ResultSetToJson;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+
+import com.google.gson.JsonObject;
 
 public class OrderDAO {
 
@@ -17,7 +23,7 @@ public class OrderDAO {
         try {
             connection = JdbcUtils.getconn();
 
-            // set order table ----- BEGIN
+            // set order table ----- BEGIN<33333333
             String order_sql = "INSERT INTO order(Order_Id, Start_Time, Type_Count, Total, Order_Status) VALUES(?, ?, ?, ?, ?);";
             preparedStatement = (PreparedStatement)connection.prepareStatement(order_sql);
             preparedStatement.setInt(1,order.getOrder_Id());
@@ -58,10 +64,64 @@ public class OrderDAO {
     public static void searchEaterHistoryOrder(int userID){}
 
     // 利用 userID 查詢 外送員 當前訂單
-    public static void searchDeliverOrder(int userID){}
+    public static void searchDeliverOrder(int userID){
+    	
+    	
+    }
 
     // 利用 userID 查詢 外送員 歷史訂單
-    public static void searchDeliverHistoryOrder(int userID){}
+    public static JsonObject searchDeliverHistoryOrder(int deliverId){
+    	Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        JsonObject jsonString = null;
+        try {
+            con = JdbcUtils.getconn();
 
-    //kfcc,
+            String search_history_sql = "SELECT History_Id, Start_Time, Total, Final_Status FROM History WHERE History_Id LIKE (SELECT Order_Id FROM customer_deliver_info WHERE Deliver_Id = ?)";
+            pst = (PreparedStatement)con.prepareStatement(search_history_sql);
+            pst.setInt(1, deliverId);
+            rs = pst.executeQuery();
+            rs.getMetaData(); //取得Query資料
+            jsonString = ResultSetToJson.ResultSetToJsonObject(rs);
+         /*while(rs.next()) {
+				int na = rs.getInt("History_Id");
+
+				System.out.println(na );
+			}*/
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            JdbcUtils.close(pst,con);
+        }
+    	return jsonString;
+    }
+    //Order_id 連結對應食物查詢
+    public static JsonObject searchDeliverHistoryOrder_Food(int orderID){
+    	Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        JsonObject jsonString = null;
+        try {
+            con = JdbcUtils.getconn();
+
+            String search_history_sql = "SELECT Food_Id, Count FROM History WHERE Order_Id = ?";
+            pst = (PreparedStatement)con.prepareStatement(search_history_sql);
+            pst.setInt(1, orderID);
+            rs = pst.executeQuery();
+            rs.getMetaData(); //取得Query資料
+            jsonString = ResultSetToJson.ResultSetToJsonObject(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            JdbcUtils.close(pst,con);
+        }
+    	return jsonString;
+    }
+    
+    //測試區
+ /*   public static void main(String args[]) {
+    	searchDeliverHistoryOrder(1);
+    	
+	}*/
 }
