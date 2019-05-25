ee4ead085dfc9640aa5fd54b1456c31f48080117
philipchang8787 <chingyao301@gmail.com>
2019-05-25 22:56:58 +0800

    增加登入的JS
    不過還是掛掉了= =
    我找看看是甚麼原因

---
+++ src/main/webapp/web/js/z_logintype.js
@@ -0,0 +1,22 @@
+window.onload =function()
+{
+    $.ajax({
+        url:'LoginServlet',
+        type:'GET',
+        dataType:'json',
+        success:function(data)
+        {
+            console.log(data);
+            var cmd =data[0];
+            if(cmd =="reload")
+            {
+                window.location.href = 'LoginDemo.html';
+            }else{
+                var userName=data.User_Name;
+                var userType =data.User_Type;
+                $('#welcome_userName')[0].innerHTML =userName;
+                $('#welcome_userType')[0].innerHTML =userType;
+            }
+        }
+    })
+}
