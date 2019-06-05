0c193358ad1d8a31e79457ea571b3e65987a4e03
陸顥壬 <f2212671555@gmail.com>
2019-06-05 20:49:59 +0800

    acount --> account

--- src/main/webapp/web/js/select_User.js
+++ src/main/webapp/web/js/select_User.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    const url = "/SFD/ModifyMemberServlet"; // test url
-    // const url = "/ShowRestInfoServlet"; // 正式 url
+    const url = "/ShowRestInfoServlet"; // 正式 url
     $.ajax({
         type: "GET",
         url: url,
@@ -9,7 +8,7 @@ $(document).ready(function(){
             for(var i = 0;i< data.length;i++){
                 var user = data[i];
                 var value = user.User_Id;
-                var name = user.account;                           //要大寫還是小寫 Account
+                var name = user.Account;                           //要大寫還是小寫 Account
                 $('#user_select').append('<option value ='+value+'>'+name+'</option>');
             }
             $('#user_select').on('change', function() {
