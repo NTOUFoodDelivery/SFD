0cc6770fb56cefde9ccb1cafbef8fa78d136dd4e
陸顥壬 <f2212671555@gmail.com>
2019-06-06 02:02:06 +0800

    when user session dead... remove from user hash map

--- src/main/java/member/util/listener/UserListener.java
+++ src/main/java/member/util/listener/UserListener.java
@@ -5,7 +5,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import member.model.daoImpl.UserDaoImpl;
 import member.util.setting.UserStatus;
@@ -14,6 +16,8 @@ import member.util.setting.UserStatus;
 @WebListener()
 public class UserListener implements ServletContextListener,
     HttpSessionListener, HttpSessionAttributeListener {
+
+  private Map<String, Long> userHashMap;
 
   // Public constructor is required by servlet spec
   public UserListener() {
@@ -25,7 +29,7 @@ public class UserListener implements ServletContextListener,
    * @param sce ServletContextEvent
    */
   public void contextInitialized(ServletContextEvent sce) {
-    Map<String, Long> userHashMap = new ConcurrentHashMap<>();
+    userHashMap = new ConcurrentHashMap<>();
     sce.getServletContext().setAttribute("userHashMap", userHashMap);
   }
 
@@ -35,8 +39,6 @@ public class UserListener implements ServletContextListener,
    * @param sce ServletContextEvent
    */
   public void contextDestroyed(ServletContextEvent sce) {
-    Map<String, Long> userHashMap = (ConcurrentHashMap) sce.getServletContext()
-        .getAttribute("userHashMap");
     UserDaoImpl userDao = new UserDaoImpl();
     for (Long userID : userHashMap.values()) { //將 使用者 轟下線
       userDao.modifyUserStatus(userID, UserStatus.OFFLINE.toString()); // 設定狀態為 下線
@@ -44,4 +46,14 @@ public class UserListener implements ServletContextListener,
     userDao = null;
   }
 
+  /**
+   * <p>when session dead close.. ,update User in db status.. .</p>
+   * @param se HttpSessionEvent
+   */
+  @Override
+  public void sessionDestroyed(HttpSessionEvent se) {
+    HttpSession session = se.getSession();
+    Long userID = (Long) session.getAttribute("userID"); // user id
+    userHashMap.remove(session.getId()); // 剔除 user hash map
+  }
 }
