ee3abe808eb6f57dbd085d859c1742bd22b9330e
JAJA <JAJA@LAPTOP-KVCQFJ1E>
2019-06-09 17:54:14 +0800

    fix

--- src/main/java/order/model/daoImpl/OrderDaoImpl.java
+++ src/main/java/order/model/daoImpl/OrderDaoImpl.java
@@ -717,11 +717,11 @@ public ArrayList<Order> searchallHistoryOrder() {
     try {
         String sql = "SELECT history.History_Id, history.Start_Time, history.Type_Count, history.Total, history.Final_Status, history.Address, history.Other, history_customer_deliver_info.Customer_Id,customer_deliver_info.Customer_Id, member.User_Name,member.Account,member.Phone_Number"
         		+ " FROM history"
-        		+"INNER JOIN history_customer_deliver_info ON history.History_Id = history_customer_deliver_info.History_Id\n" 
-                +"INNER JOIN member ON  `member`.User_Id = history_customer_deliver_info.Customer_Id" ;
-        String sql_del = "SELECT history_customer_deliver_info.Deliver_Id,customer_deliver_info.Customer_Id, member.User_Name,member.Account,member.Phone_Number"
+        		+" INNER JOIN history_customer_deliver_info ON history.History_Id = history_customer_deliver_info.History_Id\n" 
+                +" INNER JOIN member ON  `member`.User_Id = history_customer_deliver_info.Customer_Id" ;
+        String sql_del = "SELECT history_customer_deliver_info.Deliver_Id, history_customer_deliver_info.Customer_Id, member.User_Name,member.Account,member.Phone_Number"
         		+ " FROM history_customer_deliver_info" 
-                +"INNER JOIN member ON  `member`.User_Id = history_customer_deliver_info.Deliver_Id" ;
+                +" INNER JOIN member ON  `member`.User_Id = history_customer_deliver_info.Deliver_Id" ;
         preparedStatement = connection.prepareStatement(sql);
         pst = connection.prepareStatement(sql_del);
         resultSet = preparedStatement.executeQuery();
