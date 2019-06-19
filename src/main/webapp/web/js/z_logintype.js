787ababea2ee92c8b59beb498480a5eddf1a1fdd
philipchang8787 <chingyao301@gmail.com>
2019-06-19 21:25:00 +0800

    20190523

--- src/main/webapp/web/js/z_logintype.js
+++ src/main/webapp/web/js/z_logintype.js
@@ -7,15 +7,9 @@ window.onload =function()
         success:function(data)
         {
             console.log(data);
-            var cmd =data[0];
-            if(cmd =="reload")
+            if(data === null)
             {
-                window.location.href = 'LoginDemo.html';
-            }else{
-                var userName=data.User_Name;
-                var userType =data.User_Type;
-                // $('#welcome_userName')[0].innerHTML =userName;
-                // $('#welcome_userType')[0].innerHTML =userType;
+                window.location.href = '/web/login.html';
             }
         }
     })
