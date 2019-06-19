56d626a7cb49d646326d790aa8588471eb7a19d1
philipchang8787 <chingyao301@gmail.com>
2019-06-19 20:37:03 +0800

    登出

---
+++ src/main/webapp/web/js/logout.js
@@ -0,0 +1,26 @@
+function Member_logout()
+{
+    $.ajax({
+        url:' /LogoutServlet',
+        type:'GET',
+        dataType:'json',
+        success:function(data)
+        {
+            if(data.result=="SUCCESS")
+            {
+            window.location="/web/nologin.html";
+            }
+            else
+            {
+                alert("登出失敗，請稍後再試\n\n\n\n\n\n\n\n雖然這是遊戲但這可不是鬧著玩的!!!");
+
+            }
+        },
+        error:function()
+        {
+            alert("登出失敗，請稍後再試\n\n\n\n\n\n\n\n雖然這是遊戲但這可不是鬧著玩的!!!");
+
+        }
+
+    })
+}
